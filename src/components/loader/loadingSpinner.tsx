dbb0c31ee32410a5fe0b7e55962b5e57df9754e6
Japhetism <babatundeojo30@gmail.com>
2025-06-08 17:16:23 +0100

    revamp spinner

--- src/components/loader/loadingSpinner.tsx
+++ src/components/loader/loadingSpinner.tsx
@@ -1,49 +1,47 @@
 import React, { useEffect, useRef } from 'react';
 import { View, Animated, StyleSheet } from 'react-native';
 
-const LoadingSpinner: React.FC = () => {
+const LoadingSpinner = () => {
   const rotateAnim = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
-    const rotateLoop = Animated.loop(
+    rotateAnim.setValue(0);
+
+    Animated.loop(
       Animated.timing(rotateAnim, {
         toValue: 1,
         duration: 1000,
         useNativeDriver: true,
-        easing: undefined,
       })
-    );
-    rotateLoop.start();
+    ).start();
+  }, []);
 
-    return () => rotateLoop.stop();
-  }, [rotateAnim]);
-
-  const rotate = rotateAnim.interpolate({
+  const spin = rotateAnim.interpolate({
     inputRange: [0, 1],
     outputRange: ['0deg', '360deg'],
   });
 
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.spinner, { transform: [{ rotate }] }]} />
+      <Animated.View style={[styles.spinner, { transform: [{ rotate: spin }] }]} />
     </View>
   );
 };
 
-export default LoadingSpinner;
-
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1,
   },
   spinner: {
     width: 40,
     height: 40,
     borderWidth: 4,
-    borderColor: '#ccc',
-    borderTopColor: '#007bff',
+    borderColor: '#078CFF',
+    borderTopColor: '#078CFF',
     borderRadius: 20,
   },
 });
+
+export default LoadingSpinner;
